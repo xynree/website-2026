50d4a921664ba56a563d8cecdeb7071594b36ff7
Xinrui Chen <xynree@gmail.com>
2026-01-18 17:00:26 -0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import gsap from 'gsap';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		gsap.to('.box', { x: 180, duration: 1, repeat: -1, yoyo: true });
+	});
+</script>
+
+<h2>xinrui chen</h2>
+<div class="box" />
+
+<style>
+	.box {
+		width: 20px;
+		height: 20px;
+		background-color: rgb(148, 148, 195);
+	}
+</style>
